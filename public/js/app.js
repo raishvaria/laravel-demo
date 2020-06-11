3c63e9001ee0007ce364ec9d0e8c0e252e863ab5
Raish <raishvaria1996@gmail.com>
2020-06-11 10:19:31 +0530

    install tailwind css

--- public/js/app.js
+++ public/js/app.js
@@ -49935,8 +49935,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! E:\laragon\www\auth\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! E:\laragon\www\auth\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! E:\laragon\www\laravel\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! E:\laragon\www\laravel\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,90 +10,32 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
+        <div class="md:flex mx-auto container border p-4 mt-5">
+            <div class="md:flex-shrink-0">
+                <img
+                    class="rounded-lg md:w-56"
+                    src="https://images.unsplash.com/photo-1569012871812-f38ee64cd54c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=450&q=80"
+                    alt="marketing"
+                >
+            </div>
+            <div class="mt-4 md:mt-0 md:ml-6">
+                <div class="uppercase tracking-wide text-sm text-indigo-600 font-bold">
+                    Marketing
                 </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    GreeNat
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                <a
+                    href="#"
+                    class="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline"
+                >
+                    Finding Customers for your new business
+                </a>
+                <p class="mt-2 text-gray-600">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur at corrupti iure doloribus,
+                     debitis dicta voluptatibus voluptate quod?
+                    Aperiam mollitia ipsa eligendi amet delectus, quaerat officiis ut? Ea, eaque nisi.
+                </p>
             </div>
         </div>
     </body>
